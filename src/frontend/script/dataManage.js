decc20ff05621d07749ff44615ceaefedd3e5cb3
Ale96Pa <palma.1871556@studenti.uniroma1.it>
2022-04-28 18:05:14 +0200

    Initialized deviation area

--- src/frontend/script/dataManage.js
+++ src/frontend/script/dataManage.js
@@ -4,38 +4,134 @@
 //   return "Hello from " + x;
 // }
 
-const sumValues = obj => Object.values(obj).reduce((a, b) => a + b);
+function sumErrorsDeviation(data){
+    const sumMissing = data.reduce((accumulator, object) => {
+        return {
+            N: accumulator.N + object.missing.N,
+            A: accumulator.A + object.missing.A,
+            W: accumulator.W + (object.missing.W || 0),
+            R: accumulator.R + object.missing.R,
+            C: accumulator.C + object.missing.C,
+        }
+    }, {N:0, A:0, W:0, R:0, C:0});
 
-function renderBars(data){
-    // create svg element:
-    var svg = d3.select("#rect").append("svg").attr("width", 800).attr("height", 200)
+    const sumRepetition = data.reduce((accumulator, object) => {
+        return {
+            N: accumulator.N + object.repetition.N,
+            A: accumulator.A + object.repetition.A,
+            W: accumulator.W + (object.repetition.W || 0),
+            R: accumulator.R + object.repetition.R,
+            C: accumulator.C + object.repetition.C,
+        }
+    }, {N:0, A:0, W:0,R:0, C:0});
 
-    // Add the path using this helper function
-    svg.append('rect')
-    .attr('x', 10)
-    .attr('y', 120)
-    .attr('width', 600)
-    .attr('height', 40)
-    .attr('stroke', 'black')
-    .attr('fill', '#69a3b2');
+    const sumMismatch = data.reduce((accumulator, object) => {
+        return {
+            N: accumulator.N + object.mismatch.N,
+            A: accumulator.A + object.mismatch.A,
+            W: accumulator.W + (object.mismatch.W || 0),
+            R: accumulator.R + object.mismatch.R,
+            C: accumulator.C + object.mismatch.C,
+        }
+    }, {N:0, A:0, W:0, R:0, C:0});
+
+    return {missing:sumMissing, repetition:sumRepetition, mismatch: sumMismatch};
+}
+
+function renderDeviationErrorBars(objDeviations, selector){
+    const sumTotal = Object.values(objDeviations).reduce((a, b) => a + b, 0);
+    const sumN = objDeviations.N;
+    const sumA = objDeviations.A;
+    const sumW = objDeviations.W;
+    const sumR = objDeviations.R;
+    const sumC = objDeviations.C;
+
+    const height = 20;
+    const width = 950;
+
+    var svgContainerMissing = d3.select(selector).append("svg")
+    .attr("width",width)
+    .attr("height",height*3)
+    .append("g"); 
+
+    var rectangleMissing = svgContainerMissing.append("rect")
+    .attr("y", 0)
+    .attr("x",0)
+    .attr("width",sumTotal)
+    .attr("height",height)
+    .attr("style", "fill:blue");
+
+    var rectMissN = svgContainerMissing.append("rect")
+    .attr("y", 30)
+    .attr("x",0)
+    .attr("width",sumN)
+    .attr("height",height)
+    .attr("style", "fill:red");
+    var textN =sumN>0 && svgContainerMissing.append("text")
+    .attr("y", 30+(30/2))
+    .attr("x",sumN/2)
+    .text("N");
+
+    var rectMissA = svgContainerMissing.append("rect")
+    .attr("y", 30)
+    .attr("x",sumN)
+    .attr("width",sumA)
+    .attr("height",height)
+    .attr("style", "fill:green");
+    var textA = sumA >0 && svgContainerMissing.append("text")
+    .attr("y", 30+(30/2))
+    .attr("x",sumN)
+    .text("A");
+
+    var rectMissW = svgContainerMissing.append("rect")
+    .attr("y", 30)
+    .attr("x",sumN+sumA)
+    .attr("width",sumW)
+    .attr("height",height)
+    .attr("style", "fill:pink");
+    var textW = sumW>0 && svgContainerMissing.append("text")
+    .attr("y", 30+(30/2))
+    .attr("x",sumN+sumA)
+    .text("W");
+
+    var rectMissR = svgContainerMissing.append("rect")
+    .attr("y", 30)
+    .attr("x",sumN+sumA+sumW)
+    .attr("width",sumR)
+    .attr("height",height)
+    .attr("style", "fill:grey");
+    var textR = sumR>0 && svgContainerMissing.append("text")
+    .attr("y", 30+(30/2))
+    .attr("x",sumN+sumA+sumW)
+    .text("R");
+
+    var rectMissC = svgContainerMissing.append("rect")
+    .attr("y", 30)
+    .attr("x",sumN+sumA+sumW+sumR)
+    .attr("width",sumC)
+    .attr("height",height)
+    .attr("style", "fill:black");
+    var textC = sumC>0 && svgContainerMissing.append("text")
+    .attr("y", 30+(30/2))
+    .attr("x",sumN+sumA+sumW+sumR)
+    .text("C");
 }
 
 async function retrieveAlignment() {
     const alignments = await eel.alignmentsToJson()();
-    const alignmentJson = JSON.parse(alignments)
-    const entries = Object.entries(alignmentJson)
-    //console.log(entries);
-    data = []
-    for(var i=0; i<entries.length; i++){
-        const obj = entries[i][1];
-        data.push({"error": "repetition", "value": sumValues(obj.repetition)});
-    }
-    console.log(data);
+    const alignmentData = Object.entries(JSON.parse(alignments)).map(entry => {
+        return {incident_id: entry[0],
+            ...entry[1]
+        };
+    });
+    
+    const errorSum = sumErrorsDeviation(alignmentData);
 
-    /*
-    [
-        {"error": "missing", "value": 100}
-    ]
-    */
-    d3.select("body").append("span").text(alignments);
+    console.log(errorSum);
+
+    renderDeviationErrorBars(errorSum.missing, "#divMissing");
+    renderDeviationErrorBars(errorSum.repetition, "#divRepetition");
+    renderDeviationErrorBars(errorSum.mismatch, "#divMismatch");
+
+    d3.select("body").append("span").text("LOADED");
 }
